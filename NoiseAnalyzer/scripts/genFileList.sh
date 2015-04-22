362f95edd5523af971f84fea0858f9bfd2849728
cowden <christopher.cowden@ttu.edu>
2015-04-22 01:20:28 -0500

    created file to generate input file lists

---
+++ NoiseAnalyzer/scripts/genFileList.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#####
+# genFileList
+# Generate the file list from a given run number 
+# This script requires the run number and output file name
+#
+
+if [ "$#" -ne "2" ]; then
+  echo "You must provide a run number and output file"
+  echo "genFileList runNumber outputFile"
+  exit 1
+fi
+
+
+RN=$1
+OFILE=$2
+
+
+STREAM=MinimumBias
+COLLECTION=Commissioning2015
+FORMAT=RAW
+
+####
+# dumpFiles function
+# dump a list of files in dataset matching run numbers
+dumpFiles(){
+  RN1=${RN:0:3}
+  RN2=${RN:3}
+  gawkcmd="BEGIN { FS=\"/\" } { if ( \$9 == $RN1 && \$10 == $RN2 ) print \$0 }"
+  dascli.py --limit=0 --query="file dataset=${DS}" | gawk "$gawkcmd"
+}
+
+DS=$(dascli.py --limit=0 --query="dataset dataset=/$STREAM/${COLLECTION}*/${FORMAT} run=$RN" | sed -n "/\/${STREAM}/p")
+
+if [ -z "$DS" ]; then
+  echo "The dataset /${STREAM}/${COLLECTION}*/${RAW} for run=$RN cannot be found"
+  exit 2
+fi
+
+
+cat scripts/DataTemplate.txt > $OFILE
+count=0
+fileList=$(dumpFiles)
+for file in $fileList; do
+  if [ "$count" == "0" ]; then
+    echo "  '$file'" >> $OFILE
+  else
+    echo "  ,'$file'" >> $OFILE
+  fi
+  count=$((count+1))
+  if [ "$count" == "256" ]; then
+    echo "])" >> $OFILE
+    echo "readFiles.extend([" >> $OFILE
+    count=0
+  fi
+done
+echo "])" >> $OFILE
+
+
+echo "Completed generating $OFILE"
+
+
